78689487a48147e156d1f9810f6b2f84e5ba448d
gonzalofernandez <gonzalo.fernande@hotmail.com>
2017-04-26 22:29:10 +0200

    borro versiones del proyecto calculadora_vigenere

--- calculadora_vigenere/js/app.js
+++
@@ -1,3 +0,0 @@
-var cadena = prompt("Introduzca un texto escrito entre asteriscos(*):");
-var clave = prompt("Introduzca una clave:");
-document.write(vigenere(cadena, clave));
--- calculadora_vigenere_corregido/js/app.js
+++
@@ -1,2 +0,0 @@
-window.document.write(vigenere(window.prompt("Introduzca un texto entre (*) para cifrar o entre (#) para descifrar"),
-                               window.prompt("Introduzca la clave:")));
